ca0189045cc421a93fb3635f8ce54124cfe4d3b2
mariapaulav <mariapaulaviviz@gmail.com>
2022-08-16 21:24:33 -0500

    img free link updated

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export default [
     address: '3801 Chalk Butte Rd',
     product: 'Star wallet',
     image:
-    'https://cdn-icons.flaticon.com/png/512/4140/premium/4140057.png?token=exp=1660696581~hmac=7dd86662189631b583fe16aa812d65a0'
+    'https://cdn-icons-png.flaticon.com/512/4139/4139948.png'
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export default [
     address: '101 Independence Avenue',
     product: 'Rocket socks',
     image:
-      'https://cdn-icons.flaticon.com/png/512/4140/premium/4140074.png?token=exp=1660696581~hmac=e83c175bfa3c20d14e6b41f415a08d4f',
+    'https://cdn-icons-png.flaticon.com/512/2552/2552801.png',
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ export default [
     address: '4362 Hoffman Avenue',
     product: 'Animal belt',
     image:
-      'https://cdn-icons.flaticon.com/png/512/4140/premium/4140051.png?token=exp=1660696581~hmac=3f067192fddfdd3ce7a0280683a68bd0',
+      'https://cdn-icons-png.flaticon.com/512/4139/4139951.png',
   },
   {
     id: 5,
@@ -37,6 +37,6 @@ export default [
     address: '4937 Marigold Lane',
     product: 'Lemon pouch',
     image:
-      'https://cdn-icons-png.flaticon.com/512/2552/2552801.png',
+      'https://cdn-icons-png.flaticon.com/512/4139/4139970.png',
   },
 ];
